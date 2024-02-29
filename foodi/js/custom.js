b04a61835fb74ad964eeb8a067dae2d78c4c870f
ehas <ehasahamed@gmail.com>
2024-02-29 10:17:40 +0530

    testimonial slide

--- foodi/js/custom.js
+++ foodi/js/custom.js
@@ -2,5 +2,37 @@ $(document).ready(function(){
    $(".fa-bars").click(function(){
   $(".mobile-nav").slideToggle("Slow");
    });
+// testimonial
+$("#user1").click(function(){
+   $(".testimonial2").hide();
+   $(".testimonial1").show();
+   $(".testimonial3").hide();
+   $(".testimonial1").animate({  
+      left: '250px',  
+      
+        
+  });    
+ });
+ $("#user2").click(function(){
+   $(".testimonial2").show();
+   $(".testimonial1").hide();
+   $(".testimonial3").hide();
+   $(".testimonial2").animate({  
+      left: '250px',  
+      
+        
+  });     
+ });
+
+ $("#user3").click(function(){
+   $(".testimonial2").hide();
+   $(".testimonial1").hide();
+   $(".testimonial3").show();
+   $(".testimonial3").animate({  
+      left: '250px',  
+      
+        
+  });   
+ });
 });
 
